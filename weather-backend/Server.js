eed0d95fb862c06c15ff9ea9c7d3a02ef8419315
zeyad salim <ziadsalim121@gmail.com>
2026-08-03 20:33:42 +0300

    feat: initialize Express server on port 3000 with root endpoint

--- weather-backend/Server.js
+++ weather-backend/Server.js
@@ -2,10 +2,10 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send("hello world")
 })
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`server is running at ${port}`)
 });
