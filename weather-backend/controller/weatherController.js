d369c1a083ad9831cdd44e45b566779edfe2a9df
zeyad salim <ziadsalim121@gmail.com>
2026-08-05 19:00:04 +0300

    feat: implement getWeather controller to fetch weather data from service

--- weather-backend/controller/weatherController.js
+++ weather-backend/controller/weatherController.js
@@ -1,4 +1,11 @@
+const WetherService = require("../Services/weatherService");
 
+const getWeather = async (req,res) =>{
+    const data = await WetherService.getWeather();
+    
+  res.json(data);
+}
 
 module.exports = {
+    getWeather
 }
