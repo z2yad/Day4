16854cb6e1516b0d69fda488f42e3a079486032c
zeyad salim <ziadsalim121@gmail.com>
2026-08-06 14:31:47 +0300

    feat: implement WeatherApiClient with functions to fetch current, daily, and hourly weather data

--- weather-backend/clients/WeatherApiClient.js
+++ weather-backend/clients/WeatherApiClient.js
@@ -1,55 +1,59 @@
 const axios = require("axios");
 
 const getCurrentWeather = async () => {
-    const response = await axios.get(
-        "https://api.open-meteo.com/v1/forecast",
-        {
-            params: {
-                latitude: 30.0444,
-                longitude: 31.2357,
-                current: "temperature_2m,relative_humidity_2m,wind_speed_10m"
+    try {
+        const response = await axios.get(
+            "https://api.open-meteo.com/v1/forecast",
+            {
+                params: {
+                    latitude: 30.0444,
+                    longitude: 31.2357,
+                    current: "temperature_2m,relative_humidity_2m,wind_speed_10m"
+                }
             }
-        }
-    )
-    if (response.status != 200) {
-        throw new Error(`API Error: ${response.statusText}`);
+        )
+        return response.data;
+    } catch (err) {
+        console.log(err);
     }
-    return response.data;
 }
 const getForecast = async () => {
-    const response = await axios.get(
-        "https://api.open-meteo.com/v1/forecast",
-        {
-            params: {
-                latitude: 30.0444,
-                longitude: 31.2357,
-                daily: "temperature_2m_max,temperature_2m_min,weather_code"
+    try {
+        const response = await axios.get(
+            "https://api.open-meteo.com/v1/forecast",
+            {
+                params: {
+                    latitude: 30.0444,
+                    longitude: 31.2357,
+                    daily: "temperature_2m_max,temperature_2m_min,weather_code"
+                }
             }
-        }
-    )
-    if (response.status != 200) {
-        throw new Error(`API Error: ${response.statusText}`);
+        )
+        return response.data;
+    } catch (err) {
+        console.log(err);
     }
-    return response.data;
 }
 //hourly
 
 const gethourly = async () => {
-    const response = await axios.get(
-        "https://api.open-meteo.com/v1/forecast",
-        {
-            params: {
-                latitude: 30.0444,
-                longitude: 31.2357,
-                hourly: "time,temperature_2m"
+    try {
+        const response = await axios.get(
+            "https://api.open-meteo.com/v1/forecast",
+            {
+                params: {
+                    latitude: 30.0444,
+                    longitude: 31.2357,
+                    hourly: "time,temperature_2m"
+                }
             }
-        }
 
-    )
-    if (response.status !== 200) {
-        throw new Error(`API Error: ${response.statusText}`);
+        )
+
+        return response.data;
+    } catch (err) {
+        console.log(err);
     }
-    return response.data;
 }
 module.exports = {
     getCurrentWeather,
