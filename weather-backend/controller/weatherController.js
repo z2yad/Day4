659badc8b6e6dac7eaea2838866db9c2d283ea04
zeyad salim <ziadsalim121@gmail.com>
2026-08-08 21:13:39 +0300

    feat: add weather controller with endpoints for current weather, forecast, hourly data, sunrise/sunset, and precipitation

--- weather-backend/controller/weatherController.js
+++ weather-backend/controller/weatherController.js
@@ -1,26 +1,26 @@
 const WetherService = require("../Services/weatherService");
 
 const getCurrentWeather = async (req, res) => {
-  const data = await WetherService.getCurrentWeather();
+  const data = await WetherService.getCurrentWeather(30.0444,31.2357);
 
   res.json(data);
 }
 const getForecast = async (req, res) => {
-  const forecast = await WetherService.getForecast();
+  const forecast = await WetherService.getForecast(30.0444,31.2357);
 
   res.json(forecast);
 }
 const gethourly = async (req, res) => {
-const hourly = await WetherService.gethourly();
+const hourly = await WetherService.gethourly(30.0444,31.2357);
 
 res.json(hourly);
 }
 const SunriseAndSunset = async (req, res) => {
-  const sunriseAndSunset = await WetherService.SunriseAndSunset();
+  const sunriseAndSunset = await WetherService.SunriseAndSunset(30.0444,31.2357);
   res.json(sunriseAndSunset);
 }
 const precipitationData = async (req, res) => {
-  const precipitation = await WetherService.precipitationData();
+  const precipitation = await WetherService.precipitationData(30.0444,31.2357);
   res.json(precipitation);
 }
 
