66c2e2256261e0ecf4ae9e8d41f5bcbdf10b6d97
zeyad salim <ziadsalim121@gmail.com>
2026-08-08 21:18:04 +0300

    feat: implement weather controller for current, forecast, hourly, sun, and precipitation data

--- weather-backend/controller/weatherController.js
+++ weather-backend/controller/weatherController.js
@@ -1,29 +1,29 @@
 const WetherService = require("../Services/weatherService");
 
 const getCurrentWeather = async (req, res) => {
-  const {latitude, longitude} = req.query;
+  const { latitude, longitude } = req.query;
   const data = await WetherService.getCurrentWeather(latitude, longitude);
-
+ 
   res.json(data);
 }
 const getForecast = async (req, res) => {
-  const {latitude, longitude} = req.query;
+  const { latitude, longitude } = req.query;
   const forecast = await WetherService.getForecast(latitude, longitude);
 
   res.json(forecast);
 }
 const gethourly = async (req, res) => {
-  const {latitude, longitude} = req.query;
+  const { latitude, longitude } = req.query;
   const hourly = await WetherService.gethourly(latitude, longitude);
   res.json(hourly);
 }
 const SunriseAndSunset = async (req, res) => {
-  const {latitude, longitude} = req.query;
+  const { latitude, longitude } = req.query;
   const sunriseAndSunset = await WetherService.SunriseAndSunset(latitude, longitude);
   res.json(sunriseAndSunset);
 }
 const precipitationData = async (req, res) => {
-  const {latitude, longitude} = req.query;
+  const { latitude, longitude } = req.query;
   const precipitation = await WetherService.precipitationData(latitude, longitude);
   res.json(precipitation);
 }
